a461931e5ad009d3a6bbe52d43aa8090dec04646
samueleyre <samuel.eyre@hotmail.fr>
2022-12-11 22:48:50 +0100

    add year choices

--- lib/api.ts
+++ lib/api.ts
@@ -30,110 +30,130 @@ export async function fetchApi(path = '') {
 
 export async function getStats(): Promise<moneyStatsInterface> {
     const projets = await fetchProjects();
-    const invoices = await fetchInvoices();
-    const supplierInvoices = await fetchSupplierInvoices();
 
-    const invoicesIndep = invoices.filter(inv => structureProjetIds.indexOf(inv.fk_project) === -1);
-    const invoicesStructure = invoices.filter(inv => structureProjetIds.indexOf(inv.fk_project) !== -1);
-    const spendingStructure = supplierInvoices.filter(sinv => structureThirdPartiesIds.indexOf(sinv.fk_soc) !== -1);
+    const ret = {};
+    for (let year of process.env.YEARS.split(",")) {
 
-    // complete invoices
-    invoicesIndep.map((inv) => {
-        // find projets
-        const foundProjet = projets.find(pro => pro.id === inv.fk_project);
-        if (!foundProjet) {
-            console.error(`Projet of invoice ${inv.id} not found !! 🧨`)
-        }
-        inv.pourcentapportaffaire = foundProjet.array_options.options_pourcentapportaffaire ? Number(foundProjet.array_options.options_pourcentapportaffaire) / 100 : 0
-        inv.pourcentcellule = foundProjet.array_options.options_pourcentcellule ? Number(foundProjet.array_options.options_pourcentcellule) / 100 : 0
-        inv.pourcentmarquehd = foundProjet.array_options.options_pourcentmarquehd ? Number(foundProjet.array_options.options_pourcentmarquehd) / 100 : 0
-        inv.pourcentcapitaine = foundProjet.array_options.options_pourcentcapitaine ? Number(foundProjet.array_options.options_pourcentcapitaine) / 100 : 0
-        return inv;
-    })
+      year = Number(year)
+      const invoices = await fetchInvoicesByYear(year);
+      const supplierInvoices = await fetchSupplierInvoicesByYear(year);
 
-    const stats = {
-        projectsIncome: invoicesIndep
-          .reduce((total,inv) => {
-            return total + inv.total_ht;
-        }, 0),
-        capitaineIncome: Math.trunc(invoicesIndep
-          .reduce((total,inv) => {
-            return total + inv.total_ht*inv.pourcentcapitaine;
-        }, 0)),
-        celluleIncome: Math.trunc(invoicesIndep
-          .reduce((total,inv) => {
-              return total + inv.total_ht*inv.pourcentcellule;
+      const invoicesIndep = invoices.filter(inv => structureProjetIds.indexOf(inv.fk_project) === -1);
+      const invoicesStructure = invoices.filter(inv => structureProjetIds.indexOf(inv.fk_project) !== -1);
+      const spendingStructure = supplierInvoices.filter(sinv => structureThirdPartiesIds.indexOf(sinv.fk_soc) !== -1);
+
+      // complete invoices
+      invoicesIndep.map((inv) => {
+          // find projets
+          const foundProjet = projets.find(pro => pro.id === inv.fk_project);
+          if (!foundProjet) {
+              console.error(`Projet of invoice ${inv.id} not found !! 🧨`)
+          }
+          inv.pourcentapportaffaire = foundProjet.array_options.options_pourcentapportaffaire ? Number(foundProjet.array_options.options_pourcentapportaffaire) / 100 : 0
+          inv.pourcentcellule = foundProjet.array_options.options_pourcentcellule ? Number(foundProjet.array_options.options_pourcentcellule) / 100 : 0
+          inv.pourcentmarquehd = foundProjet.array_options.options_pourcentmarquehd ? Number(foundProjet.array_options.options_pourcentmarquehd) / 100 : 0
+          inv.pourcentcapitaine = foundProjet.array_options.options_pourcentcapitaine ? Number(foundProjet.array_options.options_pourcentcapitaine) / 100 : 0
+          return inv;
+      })
+
+      const stats = {
+          projectsIncome: invoicesIndep
+            .reduce((total,inv) => {
+              return total + inv.total_ht;
+          }, 0),
+          capitaineIncome: Math.trunc(invoicesIndep
+            .reduce((total,inv) => {
+              return total + inv.total_ht*inv.pourcentcapitaine;
           }, 0)),
-        apportAffaireIncome: Math.trunc(invoicesIndep
-          .reduce((total,inv) => {
-              return total + inv.total_ht*inv.pourcentapportaffaire;
-          }, 0)),
-        brandIncome: Math.trunc(invoicesIndep
-          .reduce((total,inv) => {
-              return total + inv.total_ht*inv.pourcentmarquehd;
-          }, 0)),
-        structureDetails : {
-            // communityManagement: Math.trunc(invoicesStructure.filter(inv => inv.fk_project === structureProjets.communityManagementProjet).reduce((total, inv) => {
-            //     return total + inv.total_ht;
-            // }, 0)),
-            // coworking: Math.trunc(invoicesStructure.filter(inv => inv.fk_project === structureProjets.coworking2021Projet).reduce((total, inv) => {
-            //     return total + inv.total_ht;
-            // }, 0)),
-            // newsletter: Math.trunc(invoicesStructure.filter(inv => inv.fk_project === structureProjets.newsletter2020Projet).reduce((total, inv) => {
-            //     return total + inv.total_ht;
-            // }, 0)),
-            // fonctionnement: Math.trunc(invoicesStructure.filter(inv => inv.fk_project === structureProjets.fonctionnement2020Projet || inv.fk_project === structureProjets.fonctionnement2021Projet).reduce((total, inv) => {
-            //     return total + inv.total_ht;
-            // }, 0)),
-            banque: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.banque).reduce((total, inv) => {
-                return total + inv.total_ht;
+          celluleIncome: Math.trunc(invoicesIndep
+            .reduce((total,inv) => {
+                return total + inv.total_ht*inv.pourcentcellule;
             }, 0)),
-            maif: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.maif).reduce((total, inv) => {
-                return total + inv.total_ht;
+          apportAffaireIncome: Math.trunc(invoicesIndep
+            .reduce((total,inv) => {
+                return total + inv.total_ht*inv.pourcentapportaffaire;
             }, 0)),
-            hd: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.hd).reduce((total, inv) => {
-                return total + inv.total_ht;
+          brandIncome: Math.trunc(invoicesIndep
+            .reduce((total,inv) => {
+                return total + inv.total_ht*inv.pourcentmarquehd;
             }, 0)),
-            hdParis: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.hdParis).reduce((total, inv) => {
-                return total + inv.total_ht;
-            }, 0)),
-            communication: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.communication).reduce((total, inv) => {
-                return total + inv.total_ht;
-            }, 0)),
-            visio: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.visio).reduce((total, inv) => {
-                return total + inv.total_ht;
-            }, 0)),
-            coworking: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.coworking).reduce((total, inv) => {
-                return total + inv.total_ht;
-            }, 0)),
-        }
-    };
-    return {
-        ...stats,
-        indepIncome: stats.projectsIncome - stats.celluleIncome - stats.capitaineIncome - stats.apportAffaireIncome - stats.brandIncome
+          structureDetails : {
+              // communityManagement: Math.trunc(invoicesStructure.filter(inv => inv.fk_project === structureProjets.communityManagementProjet).reduce((total, inv) => {
+              //     return total + inv.total_ht;
+              // }, 0)),
+              // coworking: Math.trunc(invoicesStructure.filter(inv => inv.fk_project === structureProjets.coworking2021Projet).reduce((total, inv) => {
+              //     return total + inv.total_ht;
+              // }, 0)),
+              // newsletter: Math.trunc(invoicesStructure.filter(inv => inv.fk_project === structureProjets.newsletter2020Projet).reduce((total, inv) => {
+              //     return total + inv.total_ht;
+              // }, 0)),
+              // fonctionnement: Math.trunc(invoicesStructure.filter(inv => inv.fk_project === structureProjets.fonctionnement2020Projet || inv.fk_project === structureProjets.fonctionnement2021Projet).reduce((total, inv) => {
+              //     return total + inv.total_ht;
+              // }, 0)),
+              banque: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.banque).reduce((total, inv) => {
+                  return total + inv.total_ht;
+              }, 0)),
+              maif: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.maif).reduce((total, inv) => {
+                  return total + inv.total_ht;
+              }, 0)),
+              hd: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.hd).reduce((total, inv) => {
+                  return total + inv.total_ht;
+              }, 0)),
+              hdParis: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.hdParis).reduce((total, inv) => {
+                  return total + inv.total_ht;
+              }, 0)),
+              communication: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.communication).reduce((total, inv) => {
+                  return total + inv.total_ht;
+              }, 0)),
+              visio: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.visio).reduce((total, inv) => {
+                  return total + inv.total_ht;
+              }, 0)),
+              coworking: Math.trunc(spendingStructure.filter(inv => inv.fk_soc === structureThirdParties.coworking).reduce((total, inv) => {
+                  return total + inv.total_ht;
+              }, 0)),
+          }
+      };
+      ret[year] = {
+          ...stats,
+          indepIncome: stats.projectsIncome - stats.celluleIncome - stats.capitaineIncome - stats.apportAffaireIncome - stats.brandIncome
+      }
     }
+    console.log(ret)
+    return ret;
 }
 
 export async function fetchProjects() {
     return mapProjects(await fetchApi("projects?sortfield=t.rowid&sortorder=ASC&limit=100"));
 }
 
-export async function fetchInvoices() {
-    return mapInvoices(await fetchApi("invoices?sortfield=t.rowid&sortorder=ASC&limit=100"));
+export async function fetchInvoicesByYear(year) {
+    return mapInvoices(
+      await fetchApi("invoices?sortfield=t.rowid&sortorder=ASC&limit=100"),
+      year
+    );
 }
 
-export async function fetchSupplierInvoices() {
-    return mapSupplierInvoices(await fetchApi("supplierinvoices?sortfield=t.rowid&sortorder=ASC&limit=100"));
+export async function fetchSupplierInvoicesByYear(year) {
+    return mapSupplierInvoices(
+      await fetchApi("supplierinvoices?sortfield=t.rowid&sortorder=ASC&limit=100"),
+      year
+    );
 }
 
 export function mapProjects(projects: projectApiInterface[]) : ProjectEntity[] {
     return projects.map((project) => new ProjectEntity(project))
 }
 
-export function mapInvoices(invoices: invoiceApiInterface[]) : InvoiceEntity[] {
-    return invoices.map((invoice) => new InvoiceEntity(invoice)).filter(inv=> inv.paye === "1" && inv.creation_year === Number(process.env.YEAR))
+export function mapInvoices(
+  invoices: invoiceApiInterface[],
+  year: int
+) : InvoiceEntity[] {
+    return invoices.map((invoice) => new InvoiceEntity(invoice)).filter(inv=> inv.paye === "1" && inv.creation_year === year)
 }
 
-export function mapSupplierInvoices(invoices: supplierInvoiceApiInterface[]) : SupplierInvoiceEntity[] {
-    return invoices.map((invoice) => new SupplierInvoiceEntity(invoice)).filter(inv=> inv.paid === "1" && inv.creation_year === Number(process.env.YEAR))
+export function mapSupplierInvoices(
+  invoices: supplierInvoiceApiInterface[],
+  year: int
+) : SupplierInvoiceEntity[] {
+    return invoices.map((invoice) => new SupplierInvoiceEntity(invoice)).filter(inv=> inv.paid === "1" && inv.creation_year === year)
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,129 +2,170 @@ import Head from 'next/head'
 import styles from '@/styles/Home.module.scss'
 import {getStats} from "../lib/api";
 import {moneyStatsInterface} from "../interfaces/stats.interface";
-
-export default function Home({ allStats }: { allStats : moneyStatsInterface }) {
-  console.log({ allStats })
-
-  return (
-    <div className={styles.container}>
-      <Head>
-        <title>Canums stats</title>
-        <meta name="description" content="Canums devient transparent !" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main className={styles.main}>
-        <h1 className={styles.mainTitle}>
-          Les Canumériques en 2021
-        </h1>
-        <div className={styles.statContainer}>
-          <div className={styles.stat}>
-            <p className={styles.statTitle}>Chiffre d'affaire</p>
-            <p className={styles.statNumber}>{allStats.projectsIncome}</p>
-          </div>
-        </div>
-        <h2>Où va l'argent ?</h2>
-        <div className={styles.statContainer}>
-          <div className={styles.stat}>
-            <p className={styles.statTitle}>Les indépendants</p>
-            <p className={styles.statNumber}>{allStats.indepIncome}</p>
-          </div>
-          <div className={styles.stat}>
-            <p className={styles.statTitle}>La cellule lyonnaise</p>
-            <p className={styles.statNumber}>{allStats.celluleIncome}</p>
-          </div>
-          <div className={styles.stat}>
-            <p className={styles.statTitle}>Les Capitaines</p>
-            <p className={styles.statNumber}>{allStats.capitaineIncome}</p>
-          </div>
-          <div className={styles.stat}>
-            <p className={styles.statTitle}>Les apporteurs d'affaires</p>
-            <p className={styles.statNumber}>{allStats.apportAffaireIncome}</p>
-          </div>
-          <div className={styles.stat}>
-            <p className={styles.statTitle}>La marque Happy Dev</p>
-            <p className={styles.statNumber}>{allStats.brandIncome}</p>
-          </div>
-        </div>
-        <h2>Les dépenses de la structure</h2>
-        <div className={styles.statContainer}>
-          {
-            allStats.structureDetails.coworking ?
-              <div className={styles.stat}>
-                <p className={styles.statTitle}>Bureaux</p>
-                <p className={styles.statNumber}>{allStats.structureDetails.coworking}</p>
-              </div> : ""
-          }
-          {
-            allStats.structureDetails.banque ?
-              <div className={styles.stat}>
-                <p className={styles.statTitle}>Banque</p>
-                <p className={styles.statNumber}>{allStats.structureDetails.banque}</p>
-              </div> : ""
-          }
-          {
-            allStats.structureDetails.maif ?
-              <div className={styles.stat}>
-                <p className={styles.statTitle}>Assurance</p>
-                <p className={styles.statNumber}>{allStats.structureDetails.maif}</p>
-              </div> : ""
-          }
-          {
-            allStats.structureDetails.visio ?
-              <div className={styles.stat}>
-                <p className={styles.statTitle}>Outil de visio</p>
-                <p className={styles.statNumber}>{allStats.structureDetails.visio}</p>
-              </div> : ""
-          }
-          {
-            allStats.structureDetails.communication ?
-              <div className={styles.stat}>
-                <p className={styles.statTitle}>Communication</p>
-                <p className={styles.statNumber}>{allStats.structureDetails.communication}</p>
-              </div> : ""
-          }
-          {
-            allStats.structureDetails.coworking ?
-              <div className={styles.stat}>
-                <p className={styles.statTitle}>Bureaux</p>
-                <p className={styles.statNumber}>{allStats.structureDetails.coworking}</p>
-              </div> : ""
-          }
-          {/*{*/}
-          {/*  allStats.structureDetails.communityManagement ?*/}
-          {/*    <div className={styles.stat}>*/}
-          {/*      <p className={styles.statTitle}>Community Management du coworking</p>*/}
-          {/*      <p className={styles.statNumber}>{allStats.structureDetails.communityManagement}</p>*/}
-          {/*    </div> : ""*/}
-          {/*}*/}
-          {/*{*/}
-          {/*  allStats.structureDetails.coworking ?*/}
-          {/*    <div className={styles.stat}>*/}
-          {/*      <p className={styles.statTitle}>Frais de bureau</p>*/}
-          {/*      <p className={styles.statNumber}>{allStats.structureDetails.coworking}</p>*/}
-          {/*    </div> : ""*/}
-          {/*}*/}
-          {/*{*/}
-          {/*  allStats.structureDetails.fonctionnement ?*/}
-          {/*    <div className={styles.stat}>*/}
-          {/*      <p className={styles.statTitle}>Frais de fonctionnement</p>*/}
-          {/*      <p className={styles.statNumber}>{allStats.structureDetails.fonctionnement}</p>*/}
-          {/*    </div> : ""*/}
-          {/*}*/}
-          {/*{*/}
-          {/*  allStats.structureDetails.newsletter ?*/}
-          {/*    <div className={styles.stat}>*/}
-          {/*      <p className={styles.statTitle}>Newsletter</p>*/}
-          {/*      <p className={styles.statNumber}>{allStats.structureDetails.newsletter}</p>*/}
-          {/*    </div> : ""*/}
-          {/*}*/}
-        </div>
+import {Component} from "react";
 
 
-      </main>
-    </div>
-  )
+
+export default class Home extends Component {
+
+  constructor({ allStats }: { allStats : {[key: int]: moneyStatsInterface} }) {
+      super({ allStats });
+      const year = new Date().getFullYear()
+      this.years = Object.keys(allStats).map((year) => Number(year));
+      console.log(this.years)
+      this.allStats = allStats;
+      this.state = {
+        year,
+        allStatsInYear : allStats[year]
+      };
+  }
+
+  changeDate = (year) => {
+    Array.from(document.getElementsByClassName("year")).forEach(element => {
+      if (Number(element.getAttribute("data-year")) === year) {
+        element.classList.add("selected");
+
+      } else {
+        element.classList.remove("selected");
+      }
+    });
+    this.setState(
+      {
+        year,
+        allStatsInYear : this.allStats[year]
+      }
+    );
+  }
+
+  render() {
+
+    const {allStatsInYear, year} = this.state;
+
+    return (
+      <div className={styles.container}>
+        <Head>
+          <title>Canums stats</title>
+          <meta name="description" content="Canums devient transparent !" />
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+
+        <main className={styles.main}>
+          <h1 className={styles.mainTitle} >
+            Les Canumériques en chiffres
+          </h1>
+          <div className={styles.yearsContainer} >
+            {this.years.map((year_it, index) =>
+              <button className={`year ${(year === year_it )? "selected" : "" }`} data-year={year_it} onClick={() => this.changeDate(year_it)}>{year_it}</button>
+            )}
+          </div>
+          <div className={styles.statContainer}>
+            <div className={styles.stat}>
+              <p className={styles.statTitle}>Chiffre d'affaire</p>
+              <p className={styles.statNumber}>{allStatsInYear.projectsIncome}</p>
+            </div>
+          </div>
+          <h2>Où va l'argent ?</h2>
+          <div className={styles.statContainer}>
+            <div className={styles.stat}>
+              <p className={styles.statTitle}>Les indépendants</p>
+              <p className={styles.statNumber}>{allStatsInYear.indepIncome}</p>
+            </div>
+            <div className={styles.stat}>
+              <p className={styles.statTitle}>La cellule lyonnaise</p>
+              <p className={styles.statNumber}>{allStatsInYear.celluleIncome}</p>
+            </div>
+            <div className={styles.stat}>
+              <p className={styles.statTitle}>Les Capitaines</p>
+              <p className={styles.statNumber}>{allStatsInYear.capitaineIncome}</p>
+            </div>
+            <div className={styles.stat}>
+              <p className={styles.statTitle}>Les apporteurs d'affaires</p>
+              <p className={styles.statNumber}>{allStatsInYear.apportAffaireIncome}</p>
+            </div>
+            <div className={styles.stat}>
+              <p className={styles.statTitle}>La marque Happy Dev</p>
+              <p className={styles.statNumber}>{allStatsInYear.brandIncome}</p>
+            </div>
+          </div>
+          <h2>Les dépenses de la structure</h2>
+          <div className={styles.statContainer}>
+            {
+              allStatsInYear.structureDetails.coworking ?
+                <div className={styles.stat}>
+                  <p className={styles.statTitle}>Bureaux</p>
+                  <p className={styles.statNumber}>{allStatsInYear.structureDetails.coworking}</p>
+                </div> : ""
+            }
+            {
+              allStatsInYear.structureDetails.banque ?
+                <div className={styles.stat}>
+                  <p className={styles.statTitle}>Banque</p>
+                  <p className={styles.statNumber}>{allStatsInYear.structureDetails.banque}</p>
+                </div> : ""
+            }
+            {
+              allStatsInYear.structureDetails.maif ?
+                <div className={styles.stat}>
+                  <p className={styles.statTitle}>Assurance</p>
+                  <p className={styles.statNumber}>{allStatsInYear.structureDetails.maif}</p>
+                </div> : ""
+            }
+            {
+              allStatsInYear.structureDetails.visio ?
+                <div className={styles.stat}>
+                  <p className={styles.statTitle}>Outil de visio</p>
+                  <p className={styles.statNumber}>{allStatsInYear.structureDetails.visio}</p>
+                </div> : ""
+            }
+            {
+              allStatsInYear.structureDetails.communication ?
+                <div className={styles.stat}>
+                  <p className={styles.statTitle}>Communication</p>
+                  <p className={styles.statNumber}>{allStatsInYear.structureDetails.communication}</p>
+                </div> : ""
+            }
+            {
+              allStatsInYear.structureDetails.coworking ?
+                <div className={styles.stat}>
+                  <p className={styles.statTitle}>Bureaux</p>
+                  <p className={styles.statNumber}>{allStatsInYear.structureDetails.coworking}</p>
+                </div> : ""
+            }
+            {/*{*/}
+            {/*  allStatsInYear.structureDetails.communityManagement ?*/}
+            {/*    <div className={styles.stat}>*/}
+            {/*      <p className={styles.statTitle}>Community Management du coworking</p>*/}
+            {/*      <p className={styles.statNumber}>{allStatsInYear.structureDetails.communityManagement}</p>*/}
+            {/*    </div> : ""*/}
+            {/*}*/}
+            {/*{*/}
+            {/*  allStatsInYear.structureDetails.coworking ?*/}
+            {/*    <div className={styles.stat}>*/}
+            {/*      <p className={styles.statTitle}>Frais de bureau</p>*/}
+            {/*      <p className={styles.statNumber}>{allStatsInYear.structureDetails.coworking}</p>*/}
+            {/*    </div> : ""*/}
+            {/*}*/}
+            {/*{*/}
+            {/*  allStatsInYear.structureDetails.fonctionnement ?*/}
+            {/*    <div className={styles.stat}>*/}
+            {/*      <p className={styles.statTitle}>Frais de fonctionnement</p>*/}
+            {/*      <p className={styles.statNumber}>{allStatsInYear.structureDetails.fonctionnement}</p>*/}
+            {/*    </div> : ""*/}
+            {/*}*/}
+            {/*{*/}
+            {/*  allStatsInYear.structureDetails.newsletter ?*/}
+            {/*    <div className={styles.stat}>*/}
+            {/*      <p className={styles.statTitle}>Newsletter</p>*/}
+            {/*      <p className={styles.statNumber}>{allStatsInYear.structureDetails.newsletter}</p>*/}
+            {/*    </div> : ""*/}
+            {/*}*/}
+          </div>
+
+
+        </main>
+      </div>
+    )
+  }
 }
 
 export async function getStaticProps() {
